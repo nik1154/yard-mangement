865438000cf46f570cf2e8a02b6aa6841b002917
Nikhil Negi <39829851+nik1154@users.noreply.github.com>
2019-04-19 17:17:52 +0530

    Add files via upload

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -20,14 +20,12 @@ export default class Example extends React.Component {
   render() {
     return (
       <ButtonDropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} >
-        <DropdownToggle caret style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width}}>
+        <DropdownToggle caret style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width, display:"flex", justifyContent:"space-between", alignItems:"center", paddingLeft:"2em", paddingRight:"2em"}}>
           {this.props.name}
         </DropdownToggle>
         <DropdownMenu>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem>{this.props.name} 1</DropdownItem>
+          <DropdownItem>{this.props.name} 2</DropdownItem>
         </DropdownMenu>
       </ButtonDropdown>
     );
--- src/InputForm.js
+++ src/InputForm.js
@@ -6,10 +6,10 @@ export default class Example extends React.Component {
     return (
       <Form>
         <FormGroup>
-          <Input type="email" name="email" id="exampleEmail" placeholder="User Name" style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width, textAlign:"center"}} />
+          <Input type="email" name="email" id="exampleEmail" placeholder="User Name" style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width, textAlign:"center", fontFamily: "'Lato', sans-serif", fontWeight: "700"}} />
         </FormGroup>
         <FormGroup>
-          <Input type="password" name="password" id="examplePassword" placeholder="Password " style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width, textAlign:"center", marginRight: this.props.margin}} />
+          <Input type="password" name="password" id="examplePassword" placeholder="Password " style={{backgroundColor:"#e6e6e6", color: "#8f8fa1 ", border:"none", borderRadius:"25px", margin:"5px", marginBottom:"10px", minHeight:"3em", width: this.props.width, textAlign:"center", marginRight: this.props.margin, fontFamily: "'Lato', sans-serif", fontWeight: "700"}} />
         </FormGroup>
        </Form>
     )
